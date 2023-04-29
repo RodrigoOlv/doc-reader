19802573293e3239647b6bbe1c224c33016dc1dd
Rodrigo Oliveira <rodrigo@santins.com.br>
2023-04-29 20:50:00 -0300

    mapeando apenas o conteúdo do doc e removendo rotas fora do escopo

--- index.js
+++ index.js
@@ -26,87 +26,11 @@ app.get('/document/:id', (req, res) => {
         res.status(500).send(err);
         return;
       }
-      res.send(result.data);
-    }
-  );
-});
+      
+      const document = result.data;
+      const content = (document.body.content ?? []).map((c) => (c.paragraph?.elements ?? []).map((e) => e.textRun?.content ?? '').join('')).join('');
 
-app.get('/documents/:documentId/history', async (req, res) => {
-  const { documentId } = req.params;
-
-  try {
-    const authClient = new google.auth.JWT({
-      email: process.env.GOOGLE_CLIENT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
-      scopes: ['https://www.googleapis.com/auth/documents'],
-    });
-
-    const docs = google.docs({
-      version: 'v1',
-      auth: authClient,
-    });
-
-    const response = await docs.documents.get({
-      documentId,
-      fields: 'documentId,revisions',
-    });
-
-    const { revisions } = response.data;
-    res.send(revisions);
-  } catch (err) {
-    console.log(`Ocorreu um erro: ${err}`);
-    res.status(500).send({ error: 'Não foi possível obter o histórico do documento.' });
-  }
-});
-
-app.post('/document', (req, res) => {
-  const docs = google.docs({
-    version: 'v1',
-    auth: authClient,
-  });
-
-  docs.documents.create(
-    {
-      title: req.body.title,
-    },
-    (err, result) => {
-      if (err) {
-        console.log(`Ocorreu um erro: ${err}`);
-        res.status(500).send(err);
-        return;
-      }
-      res.send(result.data);
-    }
-  );
-});
-
-app.put('/document/:id', (req, res) => {
-  const docs = google.docs({
-    version: 'v1',
-    auth: authClient,
-  });
-
-  docs.documents.batchUpdate(
-    {
-      documentId: req.params.id,
-      resource: {
-        requests: [
-          {
-            insertText: {
-              text: req.body.text,
-              endOfSegmentLocation: {},
-            },
-          },
-        ],
-      },
-    },
-    (err, result) => {
-      if (err) {
-        console.log(`Ocorreu um erro: ${err}`);
-        res.status(500).send(err);
-        return;
-      }
-      res.send(result.data);
+      res.send(content);
     }
   );
 });
