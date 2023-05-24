bb2749268f431ac23e140098c66c34c2c62f0707
Rodrigo Oliveira <rodrigo@santins.com.br>
2023-05-24 00:07:57 -0300

    removidas instruções fora do escopo sem uso e adicionados comentários do fluxo do código

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ require('dotenv').config();
 
 const app = express();
 
+// token de autenticação do google docs
 const authClient = new google.auth.JWT({
   email: process.env.GOOGLE_CLIENT_EMAIL,
   key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
   scopes: ['https://www.googleapis.com/auth/documents'],
 });
 
+// token de autenticação do google language
 const languageClient = new language.LanguageServiceClient({
   credentials: {
     client_email: process.env.GOOGLE_LANGUAGE_EMAIL,
@@ -22,89 +24,37 @@ const languageClient = new language.LanguageServiceClient({
   },
 });
 
-const stemmer = natural.PorterStemmer;
 
-app.get('/document/:id/:opt', async (req, res) => {
+app.get('/document/:id/', async (req, res) => {
   try {
     const docs = google.docs({
       version: 'v1',
       auth: authClient,
     });
 
+    // passa o id do documento para o método da biblioteca
     const result = await docs.documents.get({
       documentId: req.params.id,
     });
 
+    // trata o retorno do googleapi para isolar o conteúdo do texto
     const document = result.data;
     const content = (document.body.content ?? []).map((c) => (c.paragraph?.elements ?? []).map((e) => e.textRun?.content ?? '').join('')).join('');
 
-    let analysisResult;
-
-    switch( req.params.opt ) {
-      case '1':
-        analysisResult = await languageClient.annotateText({
-          document: {
-            content: content,
-            type: 'PLAIN_TEXT',
-          },
-          features: {
-            extractEntities: true,
-            extractDocumentSentiment: true,
-          },
-          encodingType: 'UTF8',
-        });
-
-        break;
-
-      case '2':
-        analysisResult = await languageClient.analyzeSentiment({
-          document: {
-            content,
-            type: 'PLAIN_TEXT',
-            language: 'pt',
-          },
-          encodingType: 'UTF8',
-          enableEntitySentiment: true,
-          enableEmotion: true
-        });
-
-        break;
-
-      case '3':
-        const tokenizer = natural.WordTokenizer();
-        analysisResult = tokenizer.tokenize(document);
-
-        break;
-
-      case '4':
-        const classifier = new natural.BayesClassifier();
-        classifier.addDocument(content, 'Referências');
-        classifier.train();
-
-        analysisResult = classifier.classify(document);
-
-        break;
-
-      case '5':
-        const openaiApiKey = process.env.OPENAI_API_KEY;
-
-        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-          messages: [{ role: 'system', content: 'You are a helpful assistant.' }, { role: 'user', content: content }],
-        }, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${openaiApiKey}`,
-          },
-        });
-
-        analysisResult = response.data.choices[0].message.content;
-
-        break;
-
-      default: analysisResult = content;
-    }
+    // passa o conteúdo do texto para o método de análise de sentimento do google language
+    const analysisResult = await languageClient.analyzeSentiment({
+      document: {
+        content,
+        type: 'PLAIN_TEXT',
+        language: 'pt',
+      },
+      encodingType: 'UTF8',
+      enableEntitySentiment: true,
+      enableEmotion: true
+    });
 
     res.send(analysisResult);
+
   } catch (err) {
     console.log(`Ocorreu um erro: ${err}`);
     res.status(500).send(err);
